5dea4b1ff459db44e5fd88a5c411e4f4fd09be5e
LIMYANGCHAE <yangchaelim@gmail.com>
2020-05-06 22:51:55 +0900

    test

---
+++ src/test/java/ne/slipp/MySlippApplicationTests.java
@@ -0,0 +1,13 @@
+package ne.slipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySlippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
